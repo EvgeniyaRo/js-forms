dfb3e230016ec60d7e73795c2d8bf4c9fc1c78dc
Evgeniya Rodina <mormonzya@gmail.com>
2019-01-19 13:15:36 +0300

    check-comment ready

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,5 +1,56 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+	(function() {
+
+		var commetnValidation = {
+			
+			valid: true,
+			init: function (){
+				//вызов внутренних функций
+				this._srtUpListeners();
+			},
+
+			_srtUpListeners: function(){
+				//скрываем по фокусу ошибку
+				$('#add_comment').on( 'focus', commetnValidation._errorNone);
+				//по сабмиту запускаем проверки и отправляем или выдаем ошибки
+				$('#form_comment').on('submit', commetnValidation._chekCommentSubmit);
+			},
+
+			//скрытие ошибки
+			_errorNone: function(){
+				$('.notify--error').css('display', 'none');
+			},
+
+			//проверка на заполненность. сделана методом jQery.
+			//и отправка формы в случае успеха с переходом на страницу успех
+			_chekCommentSubmit: function(event){
+
+				var form = $(this);
+				var inputs = form.find('textarea');
+				var commentValid = true;
+
+				$.each(inputs , function(index, val){
+					var input = $(val);
+					var value = input.val().trim();
+
+					if (value) {
+						console.log(5555);
+						$('#form_comment').unbind('submit').submit();
+
+					} else {
+						event.preventDefault();
+						$('.notify--error').css('display', 'block');
+					} 
+
+					
+				})
+
+			},
+
+
+		};
+		commetnValidation.init();
+	}());
 
 });
--- js/check-login.js
+++ js/check-login.js
@@ -1,5 +1,101 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');	
+	(function() {
+
+		var loginValidation = {
+			
+			valid: true,
+			init: function (){
+				//вызов внутренних функций
+				this._srtUpListeners();
+			},
+
+			_srtUpListeners: function(){
+				// $('.notify').css('display', 'none');
+				$('#logIn').on('submit', loginValidation._validateLogin);
+			},
+
+
+			_validateLogin: function(){
+
+				var form = $(this);
+				var inputs = form.find('input');
+				var loginValid = true;
+				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+				var validEmail = false, validPassword = false;
+
+				var userEmail = "info@mail.com",
+					userPass = "123456";
+
+				// Проверка email на заполненность
+				if ( $('#emailInput').val() === '' ) {
+					$('#emptyEmailError').show();
+					validEmail = false;
+				} else if ( !pattern.test($('#emailInput').val())) {
+					validEmail = false;
+					$('#emptyEmailError').hide();
+					$('#formatEmailError').show();
+				} else {
+					validEmail = true;
+					$('#emptyEmailError').hide();
+				}
+
+
+				// Проверка пароля на заполненность
+				if ( $('#passwordInput').val() === '' ) {
+					validPassword = false;
+					$('#emptyPasswordError').show();
+				} else {
+					validPassword = true;
+					$('#emptyPasswordError').hide();
+				}
+
+				if (validEmail == true && validPassword == true ) {
+					loginValidation.valid = true;
+				} else {
+					loginValidation.valid = false;
+				}
+
+
+
+				console.log(loginValidation.valid);
+
+				if ( loginValidation.valid == false ) {
+					event.preventDefault();
+				} else {
+					$('#logIn').unbind('submit').submit();
+
+				}
+
+
+
+
+
+				
+				// $.each(inputs , function(index, val){
+				// 	var input = $(val);
+				// 	var value = input.val().trim();
+				// 	var emptyInputError = input.siblings('#emptyInputError');
+				// 	var ErrorPasswordRecovery = input.siblings('#ErrorPasswordRecovery');
+				// 	console.log(value);
+					
+				// 	if ( value == '' ) {
+						
+				// 		// alert("нету");
+
+
+				// 	} else {
+				// 		// alert("есть");
+				// 	// 	// commentValid = false;
+				// 	// 	// $('.notify--error').css('display', 'block');
+				// 	}
+
+				// })
+			}
+
+
+		};
+		loginValidation.init();
+	}());
 
 });
