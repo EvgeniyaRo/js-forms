0891cfd2084292bb59789b1430718f452602e31e
Evgeniya Rodina <mormonzya@gmail.com>
2019-01-20 04:13:20 +0300

    fixed login

--- js/check-login.js
+++ js/check-login.js
@@ -4,45 +4,49 @@ $(document).ready(function() {
 
 		var loginValidation = {
 			
-			valid: true,
+			valid: false,
 			init: function (){
 				//вызов внутренних функций
 				this._srtUpListeners();
 			},
 
 			_srtUpListeners: function(){
+				//скрываем по фокусу ошибку
+				$('#emailInput').on( 'focus', loginValidation._errorNone);
+				$('#passwordInput').on( 'focus', loginValidation._errorNone);
+
 				$('#logIn').on('submit', loginValidation._validateLogin);
 			},
 
+			//скрытие ошибки
+			_errorNone: function(){
+				$('.notify').hide();
+			},
 
 			_validateLogin: function(){
 
 				var form = $(this);
 				var inputs = form.find('input');
-				var loginValid = true;
 				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 				var validEmail = false, validPassword = false;
 
 				var userEmail = "info@mail.com",
 					userPass = "123456";
-
+				
 				// Проверка email на заполненность
 				if ( $('#emailInput').val() === '' ) {
-					$('#emptyEmailError').show();
 					validEmail = false;
+					$('#emptyEmailError').show();
+					
 				//проверка на формат, соответствует ли регулярному выражению
 				} else if ( !pattern.test($('#emailInput').val())) {
 					validEmail = false;
 					$('#emptyEmailError').hide();
 					$('#formatEmailError').show();
-				//проверка, зарегистрирован ли в базе
-				} else if ( $('#emailInput').val() !== userEmail) {
-					validEmail = false;
-					$('#ErrorPasswordRecovery').show();
 				} else {
 					validEmail = true;
 					$('#emptyEmailError').hide();
-					$('#ErrorPasswordRecovery').hide();
+					$('#formatEmailError').hide();
 				}
 
 
@@ -50,21 +54,24 @@ $(document).ready(function() {
 				if ( $('#passwordInput').val() === '' ) {
 					validPassword = false;
 					$('#emptyPasswordError').show();
-				//проверка, зарегистрирован ли в базе и соответствует ли Email
-				} else if ( $('#passwordInput').val() == userPass && $('#emailInput').val() == userEmail) {
-						validPassword = true;
-						$('#emptyPasswordError').hide();
-						$('#ErrorPasswordRecovery').hide();
-						
 				} else {
-					validPassword = false;
-					$('#ErrorPasswordRecovery').show();
+					validPassword = true;
+					$('#emptyPasswordError').hide();
 				}
 				// если все верно - отправляем форму и переходим на страницу успех
-				if (validEmail == true && validPassword == true ) {
-					loginValidation.valid = true;
-				} else {
-					loginValidation.valid = false;
+				if (validEmail == true && validPassword == true) {
+
+					if ($('#emailInput').val() == userEmail
+						&&  $('#passwordInput').val() == userPass){
+							
+							loginValidation.valid = true;
+					
+					} else {
+						loginValidation.valid = false;
+						$('#ErrorPasswordRecovery').show();
+						$('#EmailPassNotFind').show();
+						$('#remindLink').show();
+					}
 				}
 
 				if ( loginValidation.valid == false ) {
@@ -74,6 +81,7 @@ $(document).ready(function() {
 
 				}
 
+				
 			}
 
 
